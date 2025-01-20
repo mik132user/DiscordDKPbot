fceca0c61fa4a37cbb20f61d5d6a07562a70dea4
Михаил <57863435+mik132user@users.noreply.github.com>
2025-01-20 16:57:06 +0100

    Update bot_launcher.py

--- GoslingDKPbot/bot_launcher.py
+++ GoslingDKPbot/bot_launcher.py
@@ -1,12 +1,11 @@
 import asyncio
 
-async def run_bot_3330():
+async def run_bot():
     proc = await asyncio.create_subprocess_exec('python', './3330/bot3330.py')
     await proc.wait()
     
 async def main():
-    # Запуск двух ботов
-    await asyncio.gather(run_bot_3330())
+    await asyncio.gather(run_bot())
 
 # Запуск событийного цикла
 asyncio.run(main())
